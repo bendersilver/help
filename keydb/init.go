2e721958311f18a02ea149fc92d8b1eb374876a8
bot <bendersilver@gmail.com>
2021-08-04 01:09:06 +0500

    err

--- keydb/init.go
+++ keydb/init.go
@@ -2,6 +2,7 @@ package keydb
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/go-redis/redis/v8"
 )
@@ -28,18 +29,26 @@ func HGet(key, field string) *redis.StringCmd {
 	return kdb.HGet(ctx, key, field)
 }
 
+// HSetStruct -
 func HSetStruct(key string, dst interface{}) error {
 	sf, err := newStructFields(dst)
 	if err != nil {
 		return err
 	}
+	if len(sf.RedisArgs) == 0 {
+		return fmt.Errorf("wrong number of arguments for 'hset' command")
+	}
 	err = kdb.HSet(ctx, key, sf.RedisArgs...).Err()
 	if err != nil {
 		return err
 	}
-	return kdb.HDel(ctx, key, sf.EmptyFields...).Err()
+	if len(sf.EmptyFields) > 0 {
+		return kdb.HDel(ctx, key, sf.EmptyFields...).Err()
+	}
+	return nil
 }
 
+// HGetStruct -
 func HGetStruct(key string, dst interface{}) error {
 	mp, err := kdb.HGetAll(ctx, key).Result()
 	if err != nil {
